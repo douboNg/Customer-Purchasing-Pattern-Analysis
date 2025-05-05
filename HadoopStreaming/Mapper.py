121270a0e224dcfb34cd387b3b48b47aa64d0202
Nguyễn Đại Dương <21520756@gm.uit.edu.vn>
2025-05-06 03:30:40 +0700

    Update python file

--- HadoopStreaming/Mapper.py
+++ HadoopStreaming/Mapper.py
@@ -1,16 +1,21 @@
-import sys
+#!/usr/bin/env python3
 
+import sys
+import csv
 
 for i, line in enumerate(sys.stdin):
     if i == 0 and "InvoiceNo" in line:
         continue
-    parts = line.strip().split("\t")
-    if len(parts) < 10:
+    try:
+        parts = next(csv.reader([line]))
+        if len(parts) < 10:
+            continue
+        invoice_no = parts[0]
+        quantity = int(float(parts[3]))
+        purchase_amount = float(parts[9])
+
+        print(f"INVOICE\t{invoice_no}")
+        print(f"PRODUCTS\t{quantity}")
+        print(f"SPEND\t{purchase_amount}")
+    except Exception:
         continue
-    invoice_no = parts[0]
-    quantity = int(float(parts[3]))
-    purchase_amount = float(parts[9])
-    
-    print(f"INVOICE\t{invoice_no}")
-    print(f"PRODUCTS\t{quantity}")
-    print(f"SPEND\t{purchase_amount}")
